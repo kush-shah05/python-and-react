e98fe5b4def8ddf5d260de33a393af442d68eb7a
Kush Shah <68541332+kush-shah05@users.noreply.github.com>
2024-10-07 19:36:01 -0400

    Update app.service.ts

--- backend/app.service.ts
+++ backend/app.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Welcome to the Coffee Shop Backend!';
+    return 'Welcome to the Coffee Shop Backend!!';
   }
 }
